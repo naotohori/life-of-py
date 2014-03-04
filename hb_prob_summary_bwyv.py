3ced93f7eba43b1071a3b7087fe9a877c95b2271
Naoto Hori <hori@85a-116.dhcp.umd.edu>
2014-03-04 16:08:37 -0500

    Add the first version of hb_prob_summary_bwyv.py

---
+++ hb_prob_summary_bwyv.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+import math
+
+cM_values = []
+frc_values = []
+#ihb_values = range(1,24)
+
+n_ratio = {}
+sum_ratio = {}
+sum2_ratio = {}
+sim_done = set()
+
+for l in open('dat_hb_prob.out','r'):
+    l = l.split()
+
+    cM = float(l[0])
+    frc = float(l[1])
+    rnd = l[2]
+    ihb = int(l[3])
+    ratio = float(l[4])
+
+    sim = (cM, frc)
+    key = (sim, ihb)
+
+    sim_done.add(sim)
+
+    if n_ratio.has_key(key):
+        n_ratio[key] = n_ratio[key] + 1
+        sum_ratio[key] = sum_ratio[key] + ratio
+        sum2_ratio[key] = sum2_ratio[key] + ratio * ratio
+    else:
+        n_ratio[key] = 1
+        sum_ratio[key] = ratio
+        sum2_ratio[key] = ratio * ratio
+
+    if cM not in cM_values:
+        cM_values.append(cM)
+    if frc not in frc_values:
+        frc_values.append(frc)
+
+cM_values.sort()
+frc_values.sort()
+
+for cM in cM_values:
+    for frc in frc_values:
+        sim = (cM,frc)
+        if sim not in sim_done:
+            continue
+            
+        f_out = open('hb_prob/%03i_%02i.hb_prob' % sim,'w')
+        # Stem 1
+        for ihb in range(1,6):
+            key = (sim, ihb)
+            ave = sum_ratio[key] / n_ratio[key]
+            sd = math.sqrt( sum2_ratio[key] / n_ratio[key]  - ave * ave )
+            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, ave, sd, n_ratio[key]))
+        f_out.write('\n\n')
+        # Stem 2
+        for ihb in range(6,9):
+            key = (sim, ihb)
+            ave = sum_ratio[key] / n_ratio[key]
+            sd = math.sqrt( sum2_ratio[key] / n_ratio[key]  - ave * ave )
+            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, ave, sd, n_ratio[key]))
+        f_out.write('\n\n')
+        # C14-A25
+        for ihb in range(9,11):
+            key = (sim, ihb)
+            ave = sum_ratio[key] / n_ratio[key]
+            sd = math.sqrt( sum2_ratio[key] / n_ratio[key]  - ave * ave )
+            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, ave, sd, n_ratio[key]))
+        f_out.write('\n\n')
+        # L2
+        for ihb in range(11,21):
+            key = (sim, ihb)
+            ave = sum_ratio[key] / n_ratio[key]
+            sd = math.sqrt( sum2_ratio[key] / n_ratio[key]  - ave * ave )
+            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, ave, sd, n_ratio[key]))
+        f_out.write('\n\n')
+        # L1
+        for ihb in range(21,24):
+            key = (sim, ihb)
+            ave = sum_ratio[key] / n_ratio[key]
+            sd = math.sqrt( sum2_ratio[key] / n_ratio[key]  - ave * ave )
+            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, ave, sd, n_ratio[key]))
+
+        f_out.close()
