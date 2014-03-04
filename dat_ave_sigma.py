50c4fd2cb13aae30e81159c77eeb1fcdb1530679
Naoto Hori <hori@85a-116.dhcp.umd.edu>
2014-03-04 16:13:31 -0500

    update

--- dat_ave_sigma.py
+++ dat_ave_sigma.py
@@ -12,12 +12,17 @@ import math
 COLUM_DIST = 4 - 1
 
 if __name__ == '__main__':
-    if not len(sys.argv) in (4,):
-        print 'Usage: % SCRIPT [GLOB query ('' needed)] [#step to be ignored] [output]'
+    if not len(sys.argv) in (4,5):
+        print 'Usage: % SCRIPT [GLOB query ("" needed)] [#step to be ignored] [output]'
+        print '  or : % SCRIPT [GLOB query ("" needed)] [#step to be ignored] [final step] [output]'
         sys.exit(2)
 
     files = glob.glob(sys.argv[1])
     step_ignore = int(sys.argv[2])
+    flg_final = False
+    if len(sys.argv) == 5:
+        flg_final = True
+        step_final  = int(sys.argv[3])
     filepath_out = sys.argv[-1]
 
     f_out = open(filepath_out,'w')
@@ -32,15 +37,25 @@ if __name__ == '__main__':
             step = step + 1
             if step < step_ignore:
                 continue 
+
             linesp = line.split()
             dist = float(linesp[COLUM_DIST])
             ave = ave + dist
             ave2 = ave2 + dist * dist
+            num = num + 1
+
+            if flg_final and step == step_final:
+                break
 
         f_in.close()
 
-        ave = ave / (step - step_ignore)
-        ave2 = ave2 / (step - step_ignore)
+        if num != (step - step_ignore +1):
+            print ('Warning: number step is %i in %s' % (num, filepath))
+        if num == 0:
+            continue
+            
+        ave = ave / num
+        ave2 = ave2 / num
         sigma = math.sqrt(ave2 - ave * ave)
 
         ## filepath is like "/Users/hori/bwyv/cM500/500_00100_002/Energies.dat"
