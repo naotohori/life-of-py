de8a289a6b967e1c9be4bad603377bc0f9363dec
Naoto Hori <hori@ACTs-MacBook-Pro.local>
2014-04-09 21:05:16 -0400

    Change and add scripts used in BWYV

---
+++ dat_hb_contact.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+
+# IDX TYP  CG1  NT1   CG2  NT2   DST    REF
+#   1 CAN    11 C03B    56 G18B   5.746   5.655
+#CLM_HB_IDX = 1 - 1
+#CLM_HB_TYP = 2 - 1
+#CLM_HB_CG1 = 3 - 1
+CLM_HB_NT1 = 4 - 1
+#CLM_HB_CG2 = 5 - 1
+CLM_HB_NT2 = 6 - 1
+#CLM_HB_DST = 7 - 1
+CLM_HB_REF = 8 - 1
+CLM_HB_2ND = 9 - 1
+
+CUTOFF_CONTACT_LOW = 0.8
+CUTOFF_CONTACT_HIG = 1.2
+
+import sys
+import os
+import glob
+
+if len(sys.argv) != 3:
+    print 'Usage: SCRIPT [HB file (bwyv.hb)] [dir_search]'
+    sys.exit(2)
+
+filepath_hb = sys.argv[1]
+dir_search = sys.argv[2]
+
+f_hb = open(filepath_hb, 'r')
+hbs = []
+for l in f_hb:
+    hbs.append(l.split())
+        
+''' Collect dirnames and detect Ion conc. and Forces. '''
+#dirs = glob.glob('*/*_*_*/')
+if dir_search[-1] != '/':
+    dir_search = dir_search + '/'
+dirs = glob.glob(dir_search)
+simulations = []
+for d in dirs:
+    d_sp = d.split('/')[-2].split('_')
+    cM  = d_sp[-3]
+    frc = d_sp[-2]
+    rnd = d_sp[-1]
+    type_pNcM = d.split('/')[-3][0:2]
+    if type_pNcM not in ('pN','cM'):
+        print 'Error: not pN nor cM'
+        sys.exit(2)
+    simulations.append((type_pNcM,cM,frc,rnd))
+
+
+''' Loop for each simulation set. '''
+orig_dir = os.getcwd()
+for sim in simulations:
+    type_pNcM, cM, frc, rnd = sim
+    os.chdir('%s/%s%s/%s_%s_%s' % (orig_dir, type_pNcM, cM, cM, frc, rnd))
+
+    if os.path.exists('hbcon.out') and os.path.exists('dist_C03B_G18B.out'):
+        if os.stat('hbcon.out').st_mtime >= os.stat('dist_C03B_G18B.out').st_mtime:
+            continue
+
+    ''' Open distance files'''
+    files_data = []
+    exist_all_files = True
+    for hb in hbs:
+        nt1 = hb[CLM_HB_NT1]
+        nt2 = hb[CLM_HB_NT2]
+        datafile = 'dist_%s_%s.out' % (nt1,nt2)
+        try:
+            files_data.append(open(datafile,'r'))
+        except:
+            exist_all_files = False
+            continue
+
+    if not exist_all_files:
+        continue
+
+    ''' Count number of lines'''
+    num_lines = 0
+    for line in files_data[0]:
+        if line.find('#') != -1:
+            continue
+        num_lines = num_lines + 1
+    files_data[0].seek(0)
+
+    ''' Open output file'''
+    f_out = open('hbcon.out', 'w')
+
+    iline = 0
+    while (iline < num_lines):
+        iline = iline + 1
+
+        ''' Prepare a dictionary for counting'''
+        contact_seq = []
+    
+        ''' Loop for HB pairs.'''
+        for ihb, hb in enumerate(hbs):
+            #idx = int(hb[CLM_HB_IDX])
+            #cg1 = int(hb[CLM_HB_CG1])
+            #cg2 = int(hb[CLM_HB_CG2])
+            #nt1 = hb[CLM_HB_NT1]
+            #nt2 = hb[CLM_HB_NT2]
+            ref = float(hb[CLM_HB_REF])
+            #name2nd = hb[CLM_HB_2ND]  # name of the 2ndary structure
+    
+            ''' Calc statistical data (percentage).'''
+            line = files_data[ihb].readline()
+            while line.find('#') != -1:
+                line = files_data[ihb].readline()
+    
+            d = float(line.strip())
+            if CUTOFF_CONTACT_LOW * ref <= d <= CUTOFF_CONTACT_HIG * ref:
+                contact_seq.append('1')
+            else:
+                contact_seq.append('0')
+    
+        for con in contact_seq:
+            f_out.write('%1s' % (con,))
+        f_out.write('\n')
+    
+    os.chdir(orig_dir)
+
--- hbprob_summary_bwyv.py
+++ hbprob_summary_bwyv.py
@@ -9,6 +9,8 @@ frc_values = []
 n_ratio = {}
 sum_ratio = {}
 sum2_ratio = {}
+avg_ratio = {}
+se_ratio = {}
 sim_done = set()
 
 import sys
@@ -36,11 +38,9 @@ for l in open(filepath_inp, 'r'):
     if n_ratio.has_key(key):
         n_ratio[key] = n_ratio[key] + 1
         sum_ratio[key] = sum_ratio[key] + ratio
-        sum2_ratio[key] = sum2_ratio[key] + ratio * ratio
     else:
         n_ratio[key] = 1
         sum_ratio[key] = ratio
-        sum2_ratio[key] = ratio * ratio
 
     if cM not in cM_values:
         cM_values.append(cM)
@@ -49,6 +49,44 @@ for l in open(filepath_inp, 'r'):
 
 cM_values.sort()
 frc_values.sort()
+
+''' Calc average '''
+for cM in cM_values:
+    for frc in frc_values:
+        sim = (cM,frc)
+        if sim not in sim_done:
+            continue
+
+        for ihb in range(1,24):
+            key = (sim,ihb)
+            avg_ratio[key] = sum_ratio[key] / float(n_ratio[key])
+
+''' Calc SD '''
+for l in open(filepath_inp, 'r'):
+    l = l.split()
+
+    cM = float(l[0])
+    frc = float(l[1])
+    rnd = l[2]
+    ihb = int(l[3])
+    ratio = float(l[4])
+
+    key = ((cM,frc), ihb)
+    if sum2_ratio.has_key(key):
+        sum2_ratio[key] = sum2_ratio[key] + (ratio - avg_ratio[key]) ** 2
+    else:
+        sum2_ratio[key] = (ratio - avg_ratio[key]) ** 2
+
+for cM in cM_values:
+    for frc in frc_values:
+        sim = (cM,frc)
+        if sim not in sim_done:
+            continue
+
+        for ihb in range(1,24):
+            key = (sim,ihb)
+            se_ratio[key] = (  math.sqrt( sum2_ratio[key] / float(n_ratio[key] - 1) )
+                             / math.sqrt( float(n_ratio[key]) )                       )
 
 for cM in cM_values:
     for frc in frc_values:
@@ -60,36 +98,38 @@ for cM in cM_values:
         # Stem 1
         for ihb in range(1,6):
             key = (sim, ihb)
-            ave = sum_ratio[key] / n_ratio[key]
-            sd = math.sqrt( sum2_ratio[key] / n_ratio[key]  - ave * ave )
-            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, ave, sd, n_ratio[key]))
+            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, avg_ratio[key], se_ratio[key], n_ratio[key]))
         f_out.write('\n\n')
         # Stem 2
         for ihb in range(6,9):
             key = (sim, ihb)
-            ave = sum_ratio[key] / n_ratio[key]
-            sd = math.sqrt( sum2_ratio[key] / n_ratio[key]  - ave * ave )
-            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, ave, sd, n_ratio[key]))
+            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, avg_ratio[key], se_ratio[key], n_ratio[key]))
         f_out.write('\n\n')
         # C14-A25
         for ihb in range(9,11):
             key = (sim, ihb)
-            ave = sum_ratio[key] / n_ratio[key]
-            sd = math.sqrt( sum2_ratio[key] / n_ratio[key]  - ave * ave )
-            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, ave, sd, n_ratio[key]))
+            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, avg_ratio[key], se_ratio[key], n_ratio[key]))
         f_out.write('\n\n')
         # L2
         for ihb in range(11,21):
             key = (sim, ihb)
-            ave = sum_ratio[key] / n_ratio[key]
-            sd = math.sqrt( sum2_ratio[key] / n_ratio[key]  - ave * ave )
-            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, ave, sd, n_ratio[key]))
+            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, avg_ratio[key], se_ratio[key], n_ratio[key]))
         f_out.write('\n\n')
         # L1
         for ihb in range(21,24):
             key = (sim, ihb)
-            ave = sum_ratio[key] / n_ratio[key]
-            sd = math.sqrt( sum2_ratio[key] / n_ratio[key]  - ave * ave )
-            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, ave, sd, n_ratio[key]))
+            f_out.write('%3i %7.3f %7.3f %3i\n' % (ihb, avg_ratio[key], se_ratio[key], n_ratio[key]))
 
         f_out.close()
+
+for ihb in range(1,24):
+    f_out = open('%s/HB%02i.hb_prob' % (dirpath_out,ihb),'w')
+    for frc in frc_values:
+        for cM in cM_values:
+            sim = (cM,frc)
+            if sim not in sim_done:
+                continue
+            key = (sim, ihb)
+            f_out.write('%04i %02i %7.3f %7.3f %3i\n' % (cM,frc,avg_ratio[key], se_ratio[key], n_ratio[key]))
+        f_out.write('\n\n')
+    f_out.close()
