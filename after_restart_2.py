feb6e74be4a9cd112548157ec94ffea6eb00af8b
naotohori <hori.naoto@gmail.com>
2015-05-19 18:36:58 -0400

    Utilities update

---
+++ after_restart_2.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2014/06/17
+@author: Naoto Hori
+'''
+
+import sys
+from cafysis.file_io.dcd import DcdFile
+from cafysis.file_io.ts import TsFile
+
+if __name__ == '__main__':
+    if len(sys.argv) < 5:
+        print 'Usage: % SCRIPT [name] [Dir 1] [Dir 2] ([Dir 3] ...) [output dir]'
+        sys.exit(2)
+    
+    name = sys.argv[1]
+    dirs = sys.argv[2:-1]
+    dir_out = sys.argv[-1]
+
+    # Prepare output files
+    out_ts = TsFile( '%s/%s.ts' % (dir_out, name) )
+    out_ts.open_to_write()
+
+    out_dcd = DcdFile( '%s/%s.dcd' % (dir_out, name) )
+    out_dcd.open_to_write()
+
+    nstep_total_ts = 0
+    nstep_total_dcd = 0
+    for (i_dir, d) in enumerate(dirs):
+        in_ts = TsFile( '%s/%s.ts' % (d, name) )
+        in_ts.open_to_read()
+        in_ts.read_header()
+    
+        in_dcd = DcdFile( '%s/%s.dcd' % (d, name) )
+        in_dcd.open_to_read()
+        in_dcd.read_header()
+    
+        # For the first directory
+        if i_dir == 0:
+            out_ts.header_lines = in_ts.header_lines
+            out_ts.copy_header( in_ts )
+            out_ts.write_header()
+            out_dcd.set_header( in_dcd.get_header() )
+            out_dcd.write_header()
+        else:
+            # restartの場合は、最初のフレームを除外
+            in_ts.read_onestep()
+            in_dcd.read_onestep()
+    
+        nstep_dcd = 0
+        while in_dcd.has_more_data():
+            out_dcd.write_onestep( in_dcd.read_onestep() )
+            nstep_dcd += 1
+    
+        nstep_ts = 0
+        while in_ts.has_more_data():
+            tsdata, tslines = in_ts.read_onestep()
+            out_ts.write_onestep( tslines )
+            nstep_ts += 1
+        
+        if i_dir == 0:
+            print ('TS  in %s: #frame = %i' % (d, nstep_ts))
+            print ('DCD in %s: #frame = %i' % (d, nstep_dcd))
+        else:
+            print ('TS  in %s: #frame = %i (the first step is eliminated.)' % (d, nstep_ts))
+            print ('DCD in %s: #frame = %i (the first step is eliminated.)' % (d, nstep_dcd))
+
+        if nstep_ts != nstep_dcd:
+            print( 'Warning: nstep_ts != nstep_dcd' )
+
+        nstep_total_ts += nstep_ts
+        nstep_total_dcd += nstep_dcd
+
+        in_ts.close()
+        in_dcd.close()
+    
+    print('Total # of frames in  TS output to %s: %i' % (dir_out, nstep_total_ts))
+    print('Total # of frames in DCD output to %s: %i' % (dir_out, nstep_total_dcd))
---
+++ dcd_frame_extract_as_dat.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2011/03/17 (dcd_frame_extract.py)
+Added stride mode on 2011/07/29 (dcd_frame_extract.py)
+Modified from dcd_frame_extract.py on 2011/12/20
+@author: Naoto Hori
+'''
+
+import sys
+from cafysis.file_io.dcd import DcdFile
+from cafysis.file_io.pdb import PdbFile
+
+if (not len(sys.argv) in (5, 6)):
+    print 'Usage: % SCRIPT [input DCD] [beginning (0)] [end] [output movie]'
+    print '       % SCRIPT [input DCD] [beginning (0)] [end] [stride] [output movie]'
+    sys.exit(2)
+
+frame_begin = int(sys.argv[2])
+if frame_begin < 0:
+    print 'Error: beginning frame should not less than 0'
+    sys.exit(2)
+frame_end = int(sys.argv[3])
+if (len(sys.argv) == 5) :
+    frame_stride = 1
+else:
+    frame_stride = int(sys.argv[4])
+    if frame_stride <= 0:
+        print 'The frame stride is invalid'
+        sys.exit(2)
+        
+frame_num = frame_end - frame_begin + 1
+if frame_num < 1 :
+    print 'The number of frames is invalid.'
+    sys.exit(2)
+
+
+# Output PDB
+f_out = open(sys.argv[-1],'w')
+
+
+# Open DCD and read the header
+dcd_filename = sys.argv[1]
+dcd = DcdFile(dcd_filename)
+dcd.open_to_read()
+dcd.read_header()
+
+def error_no_data() :
+    print 'The number of frames is invalid.'
+    print 'Header information:'
+    dcd.show_header()
+    sys.exit(2)
+
+# skip
+dcd.skip(frame_begin)
+#dcd.skip(frame_begin - 1)  ## 2013/10/26
+i_org = frame_begin
+
+# read and write
+icount = -1
+for i in xrange(frame_num) :
+    if not dcd.has_more_data() :
+        error_no_data()
+        
+    icount += 1
+    if icount % frame_stride == 0 :
+        icount = 0
+    else :
+        dcd.skip(1)
+        i_org += 1
+        continue
+    
+    struct = dcd.read_onestep()
+    for xyz in struct:
+        f_out.write('%f %f %f\n' % (xyz[0], xyz[1], xyz[2]))
+    i_org += 1
+
+dcd.close()
+f_out.close()
