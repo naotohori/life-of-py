f0e5900419ac2b4dfdf890fa68737a5afc613e9f
naotohori <hori.naoto@gmail.com>
2017-02-21 12:54:29 -0600

    Add contact calculate routine for homopolymer collapse analysis

--- lib_f2py/compile_f2py.sh
+++ lib_f2py/compile_f2py.sh
@@ -3,7 +3,7 @@
 #f2py -c -m py_bestfit --f90exec=/opt/intel/bin/ifort bestfit.F90 dsvdc.f
 
 # for 64-bit
-#f2py -c -m py_bestfit --fcompiler=intelem --compiler=intelem bestfit.F90 dsvdc.f
+f2py -c -m py_bestfit --fcompiler=intelem --compiler=intelem bestfit.F90 dsvdc.f
 f2py -c -m py_drid --fcompiler=intelem --compiler=intelem drid.F90
 f2py -c -m py_ddrid --fcompiler=intelem --compiler=intelem ddrid.F90
 f2py -c -m py_distance2_hist --fcompiler=intelem --compiler=intelem distance2_hist.F90
@@ -12,6 +12,7 @@ f2py -c -m py_count_bound_total --fcompiler=intelem --compiler=intelem count_bou
 f2py -c -m py_dcd_r2_histogram --fcompiler=intelem --compiler=intelem dcd_r2_histogram.F90
 f2py -c -m py_distance2_hist_nt --fcompiler=intelem --compiler=intelem distance2_hist_nt.F90
 f2py -c -m py_distance_count_within --fcompiler=intelem --compiler=intelem distance_count_within.F90
+f2py -c -m py_contact --fcompiler=intelem --compiler=intelem contact.F90
 
 # Mac
 #f2py -c -m py_drid --fcompiler=gnu95 drid.F90
