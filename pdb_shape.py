2e92a49a098e2ec9e29c8e934affadb54076622f
naotohori <hori.naoto@gmail.com>
2017-03-18 17:52:29 -0500

    Modify output format of pdb_shape.py

--- pdb_shape.py
+++ pdb_shape.py
@@ -28,6 +28,16 @@ import numpy as np
 
 if len(sys.argv) != 2:
     print 'Usage: SCRIPT [PDB]'
+    print ''
+    print 'Returns: %5i %6.3f %6.3f %6.3f % (N, Rg, D, S)'
+    print '    N  = number of particles (atoms)'
+    print '    Rg = radius of gyration'
+    print '    D  = Sphericity (0 <= D <= 1)'
+    print '          D = 0 --> perfect sphere'
+    print '          D > 0 --> anisotropic'
+    print '    S  = Spheroidal shape (-1/4 <= S <= 2)'
+    print '          S < 0 --> oblate'
+    print '          S > 0 --> prolate'
     sys.exit(2)
 
 
