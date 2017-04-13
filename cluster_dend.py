ec10ec154b68cfe152bf7497286662f09061ef57
naotohori <hori.naoto@gmail.com>
2017-04-13 11:17:53 -0500

    Update cluster_dend to not output labels (option)

--- cluster_dend.py
+++ cluster_dend.py
@@ -3,9 +3,20 @@
 import sys
 import scipy.cluster.hierarchy
 
-if len(sys.argv) != 3:
+if len(sys.argv) not in (3,4):
     print 'Usage: SCRIPT [prefix] [p (number of leaves)]'
+    print 'Usage: SCRIPT [prefix] [p (number of leaves)] [["NOLABEL"]]'
     sys.exit(2)
+
+if len(sys.argv) == 3:
+    flg_no_label = False
+else:
+    if sys.argv[3] == "NOLABEL":
+        flg_no_label = True
+    else:
+        print 'Usage: SCRIPT [prefix] [p (number of leaves)]'
+        print 'Usage: SCRIPT [prefix] [p (number of leaves)] [["NOLABEL"]]'
+        sys.exit(2)
 
 prefix = sys.argv[1]
 p = int(sys.argv[2])
@@ -21,6 +32,7 @@ plt.figure()
 plt.clf()
 scipy.cluster.hierarchy.dendrogram(z, p=p, truncate_mode='lastp',
                                 orientation='left', color_threshold=0,
+                                no_labels=flg_no_label,
                                 show_leaf_counts=True)
 plt.savefig(prefix+'.drid.cls.dend_%i.ps' % p)
 plt.figure()
