378ff14f811873b837aaf7d34d6ea2a9e0a29c3f
naotohori <hori.naoto@gmail.com>
2017-04-18 17:35:14 -0500

    Modify dcd_onlyS to be general

--- dcd_onlyS.py
+++ dcd_onlyS.py
@@ -5,12 +5,11 @@ from cafysis.file_io.dcd import DcdFile
 import copy
 import sys
 
-if len(sys.argv) != 3:
-    print 'Usage: SCRIPT [input DCD] [output DCD]'
+if len(sys.argv) != 4:
+    print 'Usage: SCRIPT [input DCD] [last index of RNA (count from 0)] [output DCD]'
     sys.exit(2)
 
-#ID_DOM_INI = int(sys.argv[2]) - 1  # 重心を求める際に必要
-#ID_DOM_END = int(sys.argv[3]) - 1
+last_mp = int(sys.argv[2]
 
 dcd = DcdFile(sys.argv[1])
 dcd.open_to_read()
@@ -18,13 +17,14 @@ dcd_out = DcdFile(sys.argv[-1])
 dcd_out.open_to_write()
 
 # header
+nres = (last_mp - last_mp % 3) / 3 + 1
 dcd.read_header()
 header = dcd.get_header()
 header_new = copy.deepcopy(header)
 header_new.nunit_real = 1
-header_new.nmp_real = 196
+header_new.nmp_real = nres
 header_new.lunit2mp = []
-header_new.lunit2mp.append(196)
+header_new.lunit2mp.append(nres)
 dcd_out.set_header(header_new)
 dcd_out.write_header()
 
@@ -35,7 +35,7 @@ while dcd.has_more_data() :
     data = dcd.read_onestep()
 
     data_S = []
-    for i in range(0,587,3):
+    for i in range(0,last_mp+1,3):
         data_S.append(data[i])
     
     dcd_out.write_onestep(data_S) 
