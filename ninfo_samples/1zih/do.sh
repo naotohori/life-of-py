f29e1722f7a9d06488905b9c8e3e9ea3a8f03da1
naotohori <hori.naoto@gmail.com>
2017-04-19 10:48:26 -0500

    ninfo samples 1zih added

---
+++ ninfo_samples/1zih/do.sh
@@ -0,0 +1,9 @@
+mkdir dssr
+x3dna-dssr -i=1zih_1.pdb -o=dssr/1zih.dssr.out --prefix=dssr/1zih.dssr --non-pair
+
+#pdb_cg2pdb.py 1zih_1.pdb 1zih.cg.pdb
+pdb_aa2cg.py 1zih_1.pdb 1zih.cg.pdb
+
+# Edit 1zih.cg.pdb to add residue 0 and 13
+
+ninfo_RNA13_pdb.py 1zih.cg.pdb 1zih.hb.list 1zih.ninfo
