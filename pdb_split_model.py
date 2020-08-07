db374efcafe988b55e7a7bfc0aa36a81c2bdadd4
naotohori <hori.naoto@gmail.com>
2020-08-07 12:49:39 -0500

    Add digit option in pdb_split_model.py

--- pdb_split_model.py
+++ pdb_split_model.py
@@ -7,18 +7,27 @@ Created on 2013/08/05
 
 import sys
 
-if len(sys.argv) != 4:
+if len(sys.argv) == 4:
+    f_in = open(sys.argv[1])
+    n_digit = int(sys.argv[2])
+    pfx = sys.argv[3]
+    offset = 0
+
+elif len(sys.argv) == 5:
+    f_in = open(sys.argv[1])
+    n_digit = int(sys.argv[2])
+    offset = int(sys.argv[3])
+    pfx = sys.argv[4]
+
+else:
     print('Usage SCRIPT [input pdb] [digit of IDs] [output prefix]')
+    print(' or   SCRIPT [input pdb] [digit of IDs] [offset of ID] [output prefix]')
     sys.exit(2)
-    
-f_in = open(sys.argv[1])
-n_digit = int(sys.argv[2])
-pfx = sys.argv[3]
 
 flg_open = False
 for l in f_in:
     if l[0:5] == 'MODEL':
-        filename = pfx + ('%0*i' % (n_digit, int(l.split()[1]))) + '.pdb'
+        filename = pfx + ('%0*i' % (n_digit, offset + int(l.split()[1]))) + '.pdb'
         f_out = open(filename,'w')
         flg_open = True
     elif (l[0:6] == 'REMARK' or l[0:4] == 'ATOM'   or
