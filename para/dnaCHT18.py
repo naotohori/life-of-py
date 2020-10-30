db1ff7f49b321154ca597d2d49ba8ab57581228b
Naoto Hori <hori.naoto@gmail.com>
2020-10-30 22:48:35 +0000

    CHT18 parameter added

---
+++ para/dnaCHT18.py
@@ -0,0 +1,33 @@
+
+class CHT18(object):
+    # U_BL
+    BL_SP = 62.59
+    BL_PS = 17.63
+    BL_SA = 44.31
+    BL_SG = 48.98
+    BL_SC = 43.25
+    BL_ST = 46.56
+
+    # U_BA
+    BA_PSP = 25.67
+    BA_SPS = 67.50
+    BA_PSA = 29.53
+    BA_PST = 39.56
+    BA_PSG = 26.28
+    BA_PSC = 35.25
+    BA_ASP = 67.32
+    BA_TSP = 93.99
+    BA_GSP = 62.94
+    BA_CSP = 77.78
+    
+    # U_ST
+    ST_DIST = 1.45
+    ST_DIH = 3.00
+    # cst_u0 is defined in another place. See <<<< CHT18_stack_param
+    
+    # U_HB
+    HB_DIST = 4.0
+    HB_ANGL = 1.5
+    HB_DIH_HBOND = 0.15
+    HB_DIH_CHAIN = 0.15
+    HB_U0 = -1.92
