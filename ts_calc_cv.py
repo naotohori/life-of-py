a89fc8b73f72f4eeb8481b1ad7e085744e4f4cef
Naoto Hori <hori.naoto@gmail.com>
2020-11-20 13:08:22 +0000

    Modify ts_calc_cv.py to deal with multiple files for same temperature

--- ts_calc_cv.py
+++ ts_calc_cv.py
@@ -22,16 +22,16 @@ if __name__ == '__main__':
     nframe_end = int(sys.argv[3])
     E_CUTOFF = float(sys.argv[4])
 
+    Ts = []
+    e_sq = {}
+    e = {}
+    n = {}
+
     for tsfile in glob.glob(sys.argv[1]):
         ts = TsFile(tsfile)
         ts.open_to_read()
         ts.read_header()
         
-        e_sq = 0.0
-        e = 0.0
-        n = 0
-        first = True
-        temp = 0.0
         iframe = 0
         while ts.has_more_data():
 
@@ -42,23 +42,24 @@ if __name__ == '__main__':
             if iframe > nframe_end:
                 break
 
-            if first:
-                temp = float(tsdata[0][ts.head_col.temp])
-                first = False
-            else:
-                if temp != float(tsdata[0][ts.head_col.temp]):
-                    print ('temperature is not identical')
-                    sys.exit(2)
-
+            T = float(tsdata[0][ts.head_col.temp])
             etot = float(tsdata[0][ts.head_col.e_tot])
             #evelo = float(tsdata[0][ts.head_col.e_velo])
+
             if etot > E_CUTOFF:
                 continue
 
-            e_sq += etot * etot
-            e += etot
-            n += 1
+            if T in Ts:
+                e_sq[T] += etot * etot
+                e[T] += etot
+                n[T] += 1
+            else:
+                Ts.append(T)
+                e_sq[T] = etot * etot
+                e[T] = etot
+                n[T] = 1
 
-        fn = float(n)
-        print(temp, ((e_sq/fn - (e/fn)**2) / (BOLTZC*temp*temp)))
+    for T in sorted(Ts):
+        fn = float(n[T])
+        print(T, ((e_sq[T]/fn - (e[T]/fn)**2) / (BOLTZC*T*T)), n[T])
 
