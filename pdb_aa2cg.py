15b3321d67e27ad7d2fe4dae8b98ba4becf8e48d
Naoto Hori <hori.naoto@gmail.com>
2021-01-03 16:53:28 +0000

    Modify pdb_aa2cg.py to start a chain with P

--- pdb_aa2cg.py
+++ pdb_aa2cg.py
@@ -7,7 +7,7 @@ import sys
 
 ATOMS_P = ('P', 'OP1', 'OP2')
 
-def aa2cg(chains_aa):
+def aa2cg(chains_aa, flg_start_P=False):
 
     chains_cg = []
     
@@ -16,7 +16,19 @@ def aa2cg(chains_aa):
     for c in chains_aa:
         c_cg = Chain()
         #xyz_O3 = None
+        flg_noS = False
+        flg_noB = False
+
         for ir, r in enumerate(c.residues):
+
+            # Check if the previous residue was normal
+            if flg_noB:
+                print("Error: there was no atoms for B, ir =", ir-1)
+                sys.exit(2)
+            if flg_noS:
+                print("Error: there was no atoms for S, ir =", ir-1)
+                sys.exit(2)
+
             xyz_P = Coord()
             nP = 0
             #if not xyz_O3 is None:
@@ -48,7 +60,10 @@ def aa2cg(chains_aa):
             res_id += 1
             r_cg = Residue()
     
-            if ir != 0:
+            if ir != 0 or flg_start_P:
+                if nP == 0:
+                    print('Error: no atoms for P, ir =', ir)
+                    sys.exit(2)
                 atom_id += 1
                 a = Atom()
                 a.serial = atom_id
@@ -59,25 +74,31 @@ def aa2cg(chains_aa):
                 a.xyz = xyz_P / float(nP)
                 r_cg.push_atom(a)
     
-            atom_id += 1
-            a = Atom()
-            a.serial = atom_id
-            a.name = ' S  '
-            a.res_name = 'R%s ' % nt
-            a.chain_id = 'A'
-            a.res_seq = res_id
-            a.xyz = xyz_S / float(nS)
-            r_cg.push_atom(a)
+            if nS > 0:
+                atom_id += 1
+                a = Atom()
+                a.serial = atom_id
+                a.name = ' S  '
+                a.res_name = 'R%s ' % nt
+                a.chain_id = 'A'
+                a.res_seq = res_id
+                a.xyz = xyz_S / float(nS)
+                r_cg.push_atom(a)
+            else:
+                flg_noS = True
     
-            atom_id += 1
-            a = Atom()
-            a.serial = atom_id
-            a.name = ' %sb '  %  nt
-            a.res_name = 'R%s ' % nt
-            a.chain_id = 'A'
-            a.res_seq = res_id
-            a.xyz = xyz_B / float(nB)
-            r_cg.push_atom(a)
+            if nB > 0:
+                atom_id += 1
+                a = Atom()
+                a.serial = atom_id
+                a.name = ' %sb '  %  nt
+                a.res_name = 'R%s ' % nt
+                a.chain_id = 'A'
+                a.res_seq = res_id
+                a.xyz = xyz_B / float(nB)
+                r_cg.push_atom(a)
+            else:
+                flg_noB = True
     
             c_cg.push_residue(r_cg)
     
@@ -90,15 +111,21 @@ if __name__ == '__main__':
     
     from cafysis.file_io.pdb import PdbFile
 
-    if len(sys.argv) != 3:
-        print('Usage: SCRIPT [input aa PDB] [output cg PDB]')
+    if len(sys.argv) not in (3,4):
+        print('Usage: SCRIPT [input aa PDB] [flag to start with P (Y/n)] [output cg PDB]')
         sys.exit(2)
+
+    flg_start_P = False
+    if len(sys.argv) == 4:
+        if sys.argv[2] == 'Y':
+            flg_start_P = True
+
 
     aa = PdbFile(sys.argv[1], 'r')
     chains = aa.read_all()
     aa.close()
 
-    chains_cg = aa2cg(chains)
+    chains_cg = aa2cg(chains, flg_start_P)
 
     cg = PdbFile(sys.argv[-1], 'w')
     cg.write_all(chains_cg)
