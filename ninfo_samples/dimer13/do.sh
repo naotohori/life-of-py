c6a01c85f2225b38ba7d6a3109db6eefe23b89cc
Naoto Hori <hori.naoto@gmail.com>
2021-01-13 21:29:40 +0000

    Add ninfo for DT13 AG dimer

---
+++ ninfo_samples/dimer13/do.sh
@@ -0,0 +1,4 @@
+ninfo_RNA.py --seq AGG --model DT13 --end5 P --end3 P DT13_dimer_AG.ninfo
+
+echo "NOTE:"
+echo "   To reproduce Figure 3 of Denesyuk & Thirumalai (2013), modify h ans s in bs-dist to be the original value before applying dG0"
