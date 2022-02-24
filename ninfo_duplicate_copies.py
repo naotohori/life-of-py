5541e380ed986a798217f7b6122510871cddab54
Naoto Hori <hori.naoto@gmail.com>
2022-02-24 00:42:53 +0000

    Add scripts to duplicate units within ninfo/pdb

---
+++ ninfo_duplicate_copies.py
@@ -0,0 +1,151 @@
+#!/usr/bin/env python
+
+import copy
+from cafysis.file_io.ninfo import NinfoFile
+from cafysis.elements.ninfo import NinfoSet
+
+Nunit_per_repeat = 2
+Nmp_per_repeat = 130   # The last mp of the unit dimer (dsRNA)
+Nrepeat = 27    # Number of repeat 
+
+ns = NinfoSet()
+
+ns_file = NinfoFile('./miR21inh_noNN.ninfo')
+ns_file.open_to_read()
+ns_file.read_all(ns)
+ns_file.close()
+
+ns_new = NinfoSet()
+
+for irepeat in range(Nrepeat):
+
+    unit_offset = irepeat * Nunit_per_repeat
+    mp_offset = irepeat * Nmp_per_repeat
+
+    for ni in ns.bondlengths:
+        ni_new = copy.deepcopy(ni)
+
+        ni_new.iunit1 += unit_offset
+        ni_new.iunit2 += unit_offset
+        ni_new.imp1 += mp_offset
+        ni_new.imp2 += mp_offset
+        
+        ns_new.bondlengths.append(ni_new)
+
+    for ni in ns.bondangles:
+        ni_new = copy.deepcopy(ni)
+
+        ni_new.iunit1 += unit_offset
+        ni_new.iunit2 += unit_offset
+        ni_new.imp1 += mp_offset
+        ni_new.imp2 += mp_offset
+        ni_new.imp3 += mp_offset
+        
+        ns_new.bondangles.append(ni_new)
+
+    for ni in ns.basestackDTs:
+        ni_new = copy.deepcopy(ni)
+
+        ni_new.iunit1 += unit_offset
+        ni_new.iunit2 += unit_offset
+        ni_new.imp1 += mp_offset
+        ni_new.imp2 += mp_offset
+        ni_new.dih1_iunit1 += unit_offset
+        ni_new.dih1_iunit2 += unit_offset
+        ni_new.dih1_imp1 += mp_offset
+        ni_new.dih1_imp2 += mp_offset
+        ni_new.dih1_imp3 += mp_offset
+        ni_new.dih1_imp4 += mp_offset
+        ni_new.dih2_iunit1 += unit_offset
+        ni_new.dih2_iunit2 += unit_offset
+        ni_new.dih2_imp1 += mp_offset
+        ni_new.dih2_imp2 += mp_offset
+        ni_new.dih2_imp3 += mp_offset
+        ni_new.dih2_imp4 += mp_offset
+        
+        ns_new.basestackDTs.append(ni_new)
+
+    for ni in ns.hbondDTs:
+        ni_new = copy.deepcopy(ni)
+
+        ni_new.iunit1 += unit_offset
+        ni_new.iunit2 += unit_offset
+        ni_new.imp1 += mp_offset
+        ni_new.imp2 += mp_offset
+        ni_new.ang1_iunit1 += unit_offset
+        ni_new.ang1_iunit2 += unit_offset
+        ni_new.ang1_imp1 += mp_offset
+        ni_new.ang1_imp2 += mp_offset
+        ni_new.ang1_imp3 += mp_offset
+        ni_new.ang2_iunit1 += unit_offset
+        ni_new.ang2_iunit2 += unit_offset
+        ni_new.ang2_imp1 += mp_offset
+        ni_new.ang2_imp2 += mp_offset
+        ni_new.ang2_imp3 += mp_offset
+        ni_new.dih0_iunit1 += unit_offset
+        ni_new.dih0_iunit2 += unit_offset
+        ni_new.dih0_imp1 += mp_offset
+        ni_new.dih0_imp2 += mp_offset
+        ni_new.dih0_imp3 += mp_offset
+        ni_new.dih0_imp4 += mp_offset
+        ni_new.dih1_iunit1 += unit_offset
+        ni_new.dih1_iunit2 += unit_offset
+        ni_new.dih1_imp1 += mp_offset
+        ni_new.dih1_imp2 += mp_offset
+        ni_new.dih1_imp3 += mp_offset
+        ni_new.dih1_imp4 += mp_offset
+        ni_new.dih2_iunit1 += unit_offset
+        ni_new.dih2_iunit2 += unit_offset
+        ni_new.dih2_imp1 += mp_offset
+        ni_new.dih2_imp2 += mp_offset
+        ni_new.dih2_imp3 += mp_offset
+        ni_new.dih2_imp4 += mp_offset
+        
+        ns_new.hbondDTs.append(ni_new)
+
+    for ni in ns.tertiarystackDTs:
+        ni_new = copy.deepcopy(ni)
+
+        ni_new.iunit1 += unit_offset
+        ni_new.iunit2 += unit_offset
+        ni_new.imp1 += mp_offset
+        ni_new.imp2 += mp_offset
+        ni_new.ang1_iunit1 += unit_offset
+        ni_new.ang1_iunit2 += unit_offset
+        ni_new.ang1_imp1 += mp_offset
+        ni_new.ang1_imp2 += mp_offset
+        ni_new.ang1_imp3 += mp_offset
+        ni_new.ang2_iunit1 += unit_offset
+        ni_new.ang2_iunit2 += unit_offset
+        ni_new.ang2_imp1 += mp_offset
+        ni_new.ang2_imp2 += mp_offset
+        ni_new.ang2_imp3 += mp_offset
+        ni_new.dih0_iunit1 += unit_offset
+        ni_new.dih0_iunit2 += unit_offset
+        ni_new.dih0_imp1 += mp_offset
+        ni_new.dih0_imp2 += mp_offset
+        ni_new.dih0_imp3 += mp_offset
+        ni_new.dih0_imp4 += mp_offset
+        ni_new.dih1_iunit1 += unit_offset
+        ni_new.dih1_iunit2 += unit_offset
+        ni_new.dih1_imp1 += mp_offset
+        ni_new.dih1_imp2 += mp_offset
+        ni_new.dih1_imp3 += mp_offset
+        ni_new.dih1_imp4 += mp_offset
+        ni_new.dih2_iunit1 += unit_offset
+        ni_new.dih2_iunit2 += unit_offset
+        ni_new.dih2_imp1 += mp_offset
+        ni_new.dih2_imp2 += mp_offset
+        ni_new.dih2_imp3 += mp_offset
+        ni_new.dih2_imp4 += mp_offset
+        
+        ns_new.tertiarystackDTs.append(ni_new)
+
+ns_new.update_info()
+ns_new.reassign_id()
+
+
+ns_file = NinfoFile('./new.ninfo')
+ns_file.open_to_write()
+ns_file.write_all(ns_new)
+ns_file.close()
---
+++ pdb_duplicate_copies.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+from cafysis.file_io.pdb import PdbFile
+from cafysis.elements.pdb import Chain, Residue, Atom
+import copy
+import sys
+
+Nx = 3
+Ny = 3
+Nz = 3
+Lx = 100.0
+Ly = 100.0
+Lz = 100.0
+
+pdb = PdbFile('./miR21inh.cg.pdb')
+pdb.open_to_read()
+chains = pdb.read_all()
+pdb.close()
+
+newchains = []
+
+for i in range(Nx):
+    for j in range(Ny):
+        for k in range(Nz):
+
+            dx = i * Lx
+            dy = j * Ly
+            dz = k * Lz
+
+            for c in chains:
+                c_new = Chain()
+
+                for r in c.residues:
+                    r_new = Residue()
+
+                    for a in r.atoms:
+                        a_new = copy.deepcopy(a)
+
+                        a_new.xyz.x = a.xyz.x + dx
+                        a_new.xyz.y = a.xyz.y + dy
+                        a_new.xyz.z = a.xyz.z + dz
+
+                        r_new.push_atom(a_new)
+
+                    c_new.push_residue(r_new)
+
+                newchains.append(c_new)
+
+pdb = PdbFile('miR21inh.cg.27L100.pdb')
+pdb.open_to_write()
+pdb.write_all(newchains)
+pdb.close()
