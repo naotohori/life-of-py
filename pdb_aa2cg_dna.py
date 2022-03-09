944cddc95351f3f91e03720e0701adb51c99ee81
Naoto Hori <hori.naoto@gmail.com>
2022-03-09 11:25:47 +0000

    add argparse to pdb_aa2cg_dna

--- pdb_aa2cg_dna.py
+++ pdb_aa2cg_dna.py
@@ -1,17 +1,10 @@
 #!/usr/bin/env python
 
-from lop.file_io.pdb import PdbFile
 from lop.elements.pdb import Chain, Residue, Atom
 from lop.elements.coord import Coord
 from lop.para.mass import ATOM_MASS
 
 import sys
-
-if len(sys.argv) != 3:
-    print('Usage: SCRIPT [input aa PDB] [output cg PDB]')
-    sys.exit(2)
-
-## The 5′terminal does not have the phosphatebead.
 
 ## If residues in the original PDB are
 # DT5, DA, DC ...., then
@@ -28,119 +21,141 @@ ATOMS_G = ("N9", "C8", "N7", "C5", "C6", "O6", "N1", "C2", "N2", "N3", "C4")
 ATOMS_C = ("N1", "C2", "O2", "N3", "C4", "N4", "C5", "C6")
 ATOMS_T = ("N1", "C2", "O2", "N3", "C4", "O4", "C5", "C7", "C6")
 
-aa = PdbFile(sys.argv[1])
-aa.open_to_read()
-chains = aa.read_all()
 
-cg_chains = []
+def aa2cg_dna(chains, flg_start_P=False):
 
-atom_id = 0
-for c in chains:
-    c_cg = Chain()
-    xyz_O3 = None
-    res_id = 0
+    cg_chains = []
 
-    for ir, r in enumerate(c.residues):
+    atom_id = 0
+    for c in chains:
+        c_cg = Chain()
+        xyz_O3 = None
+        res_id = 0
 
-        xyz_P = Coord()
-        nP = 0
+        for ir, r in enumerate(c.residues):
 
-        if xyz_O3 is None:
-            if ir != 0:
-                print("Error: cannot find O3 from the previous nucleotide")
-                sys.exit(2)
-        else:
-            xyz_P += xyz_O3 * ATOM_MASS["O"]
-            nP += ATOM_MASS["O"]
-            xyz_O3 = None
+            xyz_P = Coord()
+            nP = 0
 
-        xyz_S = Coord()
-        nS = 0 
-        xyz_B = Coord()
-        nB = 0
-
-        for a in r.atoms:
-            name = a.name.strip()
-
-            if name[0] == 'H':
-                continue
-
-            elif (name == "O3'") or (name == "O3*"):
-                xyz_O3 = a.xyz
-
-            elif (name in ATOMS_P) or (name in ATOMS_P_AMBER) :
-                if name.find("O") != -1:
-                    xyz_P += a.xyz * ATOM_MASS["O"]
-                    nP += ATOM_MASS["O"]
-                elif name[0] == 'P':
-                    xyz_P += a.xyz * ATOM_MASS["P"]
-                    nP += ATOM_MASS["P"]
-
-            elif (name in ATOMS_S) or (name in ATOMS_S_AMBER) :
-                if name.find("C") != -1:
-                    xyz_S += a.xyz * ATOM_MASS["C"]
-                    nS += ATOM_MASS["C"]
-                else:
-                    xyz_S += a.xyz * ATOM_MASS["O"]
-                    nS += ATOM_MASS["O"]
-
+            if xyz_O3 is None:
+                if ir != 0:
+                    print("Error: cannot find O3 from the previous nucleotide")
+                    sys.exit(2)
             else:
-                if name.find("N") != -1:
-                    xyz_B += a.xyz * ATOM_MASS["N"]
-                    nB += ATOM_MASS["N"]
-                elif name.find("C") != -1:
-                    xyz_B += a.xyz * ATOM_MASS["C"]
-                    nB += ATOM_MASS["C"]
-                elif name.find("O") != -1:
-                    xyz_B += a.xyz * ATOM_MASS["O"]
-                    nB += ATOM_MASS["O"]
+                xyz_P += xyz_O3 * ATOM_MASS["O"]
+                nP += ATOM_MASS["O"]
+                xyz_O3 = None
 
-            nt = a.res_name.strip()
-            nchain = a.chain_id.strip()
+            xyz_S = Coord()
+            nS = 0 
+            xyz_B = Coord()
+            nB = 0
 
-        res_id += 1
-        r_cg = Residue()
+            for a in r.atoms:
+                name = a.name.strip()
 
-        if ir != 0:
+                if name[0] == 'H':
+                    continue
+
+                elif (name == "O3'") or (name == "O3*"):
+                    xyz_O3 = a.xyz
+
+                elif (name in ATOMS_P) or (name in ATOMS_P_AMBER) :
+                    if name.find("O") != -1:
+                        xyz_P += a.xyz * ATOM_MASS["O"]
+                        nP += ATOM_MASS["O"]
+                    elif name[0] == 'P':
+                        xyz_P += a.xyz * ATOM_MASS["P"]
+                        nP += ATOM_MASS["P"]
+
+                elif (name in ATOMS_S) or (name in ATOMS_S_AMBER) :
+                    if name.find("C") != -1:
+                        xyz_S += a.xyz * ATOM_MASS["C"]
+                        nS += ATOM_MASS["C"]
+                    else:
+                        xyz_S += a.xyz * ATOM_MASS["O"]
+                        nS += ATOM_MASS["O"]
+
+                else:
+                    if name.find("N") != -1:
+                        xyz_B += a.xyz * ATOM_MASS["N"]
+                        nB += ATOM_MASS["N"]
+                    elif name.find("C") != -1:
+                        xyz_B += a.xyz * ATOM_MASS["C"]
+                        nB += ATOM_MASS["C"]
+                    elif name.find("O") != -1:
+                        xyz_B += a.xyz * ATOM_MASS["O"]
+                        nB += ATOM_MASS["O"]
+
+                nt = a.res_name.strip()
+                nchain = a.chain_id.strip()
+
+            res_id += 1
+            r_cg = Residue()
+
+            if ir != 0:
+                atom_id += 1
+                a = Atom()
+                a.serial = atom_id
+                a.name = ' P  '
+                a.res_name = 'D%s ' % nt[SEQ_POSITION:SEQ_POSITION+1]
+                a.chain_id = '%s' % nchain
+                a.res_seq = res_id
+                a.xyz = xyz_P / float(nP)
+                r_cg.push_atom(a)
+
             atom_id += 1
             a = Atom()
             a.serial = atom_id
-            a.name = ' P  '
+            a.name = ' S  '
             a.res_name = 'D%s ' % nt[SEQ_POSITION:SEQ_POSITION+1]
+            #a.chain_id = 'A'
             a.chain_id = '%s' % nchain
             a.res_seq = res_id
-            a.xyz = xyz_P / float(nP)
+            a.xyz = xyz_S / float(nS)
             r_cg.push_atom(a)
 
-        atom_id += 1
-        a = Atom()
-        a.serial = atom_id
-        a.name = ' S  '
-        a.res_name = 'D%s ' % nt[SEQ_POSITION:SEQ_POSITION+1]
-        #a.chain_id = 'A'
-        a.chain_id = '%s' % nchain
-        a.res_seq = res_id
-        a.xyz = xyz_S / float(nS)
-        r_cg.push_atom(a)
+            atom_id += 1
+            a = Atom()
+            a.serial = atom_id
+            #a.name = ' %sb '  %  nt
+            a.name = ' %sb '  %  nt[SEQ_POSITION:SEQ_POSITION+1]
+            #a.res_name = 'R%s ' % nt
+            a.res_name = 'D%s ' % nt[SEQ_POSITION:SEQ_POSITION+1]
+            #a.chain_id = 'A'
+            a.chain_id = '%s' % nchain
+            a.res_seq = res_id
+            a.xyz = xyz_B / float(nB)
+            r_cg.push_atom(a)
 
-        atom_id += 1
-        a = Atom()
-        a.serial = atom_id
-        #a.name = ' %sb '  %  nt
-        a.name = ' %sb '  %  nt[SEQ_POSITION:SEQ_POSITION+1]
-        #a.res_name = 'R%s ' % nt
-        a.res_name = 'D%s ' % nt[SEQ_POSITION:SEQ_POSITION+1]
-        #a.chain_id = 'A'
-        a.chain_id = '%s' % nchain
-        a.res_seq = res_id
-        a.xyz = xyz_B / float(nB)
-        r_cg.push_atom(a)
+            c_cg.push_residue(r_cg)
 
-        c_cg.push_residue(r_cg)
-        
-    cg_chains.append(c_cg)
+        cg_chains.append(c_cg)
 
-cg = PdbFile(sys.argv[-1])
-cg.open_to_write()
-cg.write_all(cg_chains)
-cg.close()
+    return cg_chains
+
+
+if __name__ == '__main__':
+
+    from cafysis.file_io.pdb import PdbFile
+
+    if len(sys.argv) not in (3,4):
+        print('Usage: SCRIPT [input aa PDB] [flag to start with P (Y/n)] [output cg PDB]')
+        sys.exit(2)
+
+    flg_start_P = False
+    if len(sys.argv) == 4:
+        if sys.argv[2] == 'Y':
+            flg_start_P = True
+
+
+    aa = PdbFile(sys.argv[1], 'r')
+    chains = aa.read_all()
+    aa.close()
+
+    chains_cg = aa2cg_dna(chains, flg_start_P)
+
+    cg = PdbFile(sys.argv[-1], 'w')
+    cg.write_all(chains_cg)
+    cg.close()
+
