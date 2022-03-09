5d3defabc11f5d00b79d44420f7d22d3da8f0c37
Naoto Hori <hori.naoto@gmail.com>
2022-03-09 11:22:20 +0000

    Modify sisbp_cat.py for the format changes in bp file

--- sisbp_cat.py
+++ sisbp_cat.py
@@ -8,10 +8,10 @@ f.open_to_read()
 
 while f.has_more_data():
 
-    pairs = f.read_onestep()
+    pairs, energies = f.read_onestep()
 
-    for p in pairs:
-        print(f" ({p[0]:d}, {p[1]:d})", end='')
+    for p,e in zip(pairs, energies):
+        print(f" ({p[0]:d}, {p[1]:d}: {e:f})", end='')
     print("")
 
 f.close()
