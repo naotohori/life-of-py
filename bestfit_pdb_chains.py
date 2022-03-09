c239cbe0f0165c1573e7aae941fe579b32b809f5
Naoto Hori <hori.naoto@gmail.com>
2022-03-09 11:27:26 +0000

    add argparse to bestfit_pdb_chains

--- bestfit_pdb_chains.py
+++ bestfit_pdb_chains.py
@@ -28,9 +28,16 @@ def apply_rot(chains, rot):
 if __name__ == '__main__':
 
     import sys
-    if len(sys.argv) != 3:
-        print ("Usage: SCRIPT [input reference PDB] [input query PDB]")
-        sys.exit(2)
+    import argparse
+
+    parser = argparse.ArgumentParser(
+             description='Calculate RMSD and transformation matrix',
+             formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('pdb_ref', help='input reference PDB')
+    parser.add_argument('pdb_que', help='input query PDB')
+    parser.add_argument('--mtx', dest='file_mtx', help='output matrix (mtx) file')
+    args = parser.parse_args()
 
     chains_ref = PdbFile(sys.argv[1],'r').read_all_and_close()
     chains_que = PdbFile(sys.argv[2],'r').read_all_and_close()
@@ -39,4 +46,12 @@ if __name__ == '__main__':
 
     print(rmsd)
     print(rot)
+
+    if args.file_mtx is not None:
+        f_out = open(args.file_mtx,'w')
+        f_out.write('#matrix\n')
+        for i in range(4):
+            for j in range(4):
+                f_out.write(' %f' % (rot[i,j],))
+            f_out.write('\n')
 
