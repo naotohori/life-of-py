52c72d80f4d255c7559f61c52ab00b0c9d41fc26
Naoto Hori <hori.naoto@gmail.com>
2023-02-25 17:05:39 +0000

    Improve dcd_traj_distance_PBC so that a cuboid box can be used

--- dcd_traj_distance_PBC.py
+++ dcd_traj_distance_PBC.py
@@ -9,20 +9,19 @@ import sys
 import math
 from lop.file_io.dcd import DcdFile
 
-if len(sys.argv) != 6:
-    print(' Usage: % SCRIPT [input DCD] [boxsize] [ID1] [ID2] [output PDB] ')
+if len(sys.argv) != 8:
+    print(' Usage: % SCRIPT [input DCD] [boxsize x y z] [ID1] [ID2] [output] ')
     sys.exit(2)
     
 dcd = DcdFile(sys.argv[1])
-boxsize = float(sys.argv[2])
-id1 = int(sys.argv[3]) - 1
-id2 = int(sys.argv[4]) - 1
+box = [float(sys.argv[2]), float(sys.argv[3]), float(sys.argv[4])]
+id1 = int(sys.argv[5]) - 1
+id2 = int(sys.argv[6]) - 1
 
 dcd.open_to_read()
 dcd.read_header()
 
-box = [boxsize]*3
-half = [0.5*boxsize]*3
+half = [0.5 * x for x in box]
 
 def pb_neighbor(v):
     for i in range(3):
@@ -41,9 +40,11 @@ while dcd.has_more_data() :
          data[id1][1] - data[id2][1],
          data[id1][2] - data[id2][2] ]
 
+    print(v)
     v = pb_neighbor(v)
 
     d = math.sqrt( v[0]**2 + v[1]**2 + v[2]**2 )
+    print(v, d)
 
     f_out.write('%.2f\n' % d)
 dcd.close()
